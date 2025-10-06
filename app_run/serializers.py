361114785e01179c4397961dc677840366221b77
andrei <freemail_2019@mail.ru>
2025-10-06 13:58:08 +0800

    add field 'type'

--- app_run/serializers.py
+++ app_run/serializers.py
@@ -15,7 +15,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'last_name', 'first_name', 'type']
+        fields = ['id', 'date_joined', 'username', 'last_name', 'first_name', 'type']
 
     def get_type(self, obj):
         text = ''
