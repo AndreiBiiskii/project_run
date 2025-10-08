3057af13eee3650cd4745e0dc5783b8734d2a7f2
andrei <freemail_2019@mail.ru>
2025-10-08 13:36:05 +0800

    add search by ['first_name', 'last_name']

--- app_run/serializers.py
+++ app_run/serializers.py
@@ -5,19 +5,19 @@ from app_run.models import Run
 
 
 class UserSerializer(serializers.ModelSerializer):
-    # type = serializers.SerializerMethodField()
+    type = serializers.SerializerMethodField()
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'last_name', 'first_name']
+        fields = ['id', 'username', 'last_name', 'first_name', 'type']
 
-    # def get_type(self, obj):
-    #     text = ''
-    #     if not obj.is_staff:
-    #         text = 'athlete'
-    #     if obj.is_staff:
-    #         text = 'coach'
-    #     return text
+    def get_type(self, obj):
+        text = ''
+        if not obj.is_staff:
+            text = 'athlete'
+        if obj.is_staff:
+            text = 'coach'
+        return text
 
 
 class AthleteSerializer(serializers.ModelSerializer):
