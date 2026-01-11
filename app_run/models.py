ed6bcd8bdc6f42f2752be4865f0fc67cfa03f19a
andrei <freemail_2019@mail.ru>
2026-01-11 15:19:31 +0800

    default=timezone.now

--- app_run/models.py
+++ app_run/models.py
@@ -6,7 +6,7 @@ from project_run.settings.base import CHOICES_STATUS
 
 
 class Run(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now=True)
     athlete = models.ForeignKey(User, on_delete=models.CASCADE)
     comment = models.TextField()
     status = models.CharField(choices=CHOICES_STATUS, default=False)
