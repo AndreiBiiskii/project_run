a6b2e773b99673fa3502ffade51775f029f043b3
andrei <freemail_2019@mail.ru>
2026-01-11 16:36:01 +0800

    default=timezone.now

--- app_run/models.py
+++ app_run/models.py
@@ -38,7 +38,7 @@ class Position(models.Model):
     run = models.ForeignKey(Run, on_delete=models.CASCADE)
     latitude = models.FloatField()
     longitude = models.FloatField()
-    date_time = models.DateTimeField(default=datetime.datetime.now)
+    date_time = models.DateTimeField(default=datetime.datetime.now(datetime.timezone.utc))
     speed = models.FloatField(default=0.0)
     distance = models.FloatField(default=0.0)
 
