50cbb129f149cbaa7b9390fe04532fe625355ce0
andrei <freemail_2019@mail.ru>
2026-03-19 22:37:05 +0800

    2 километра за 10 минут! v100500

--- app_run/views.py
+++ app_run/views.py
@@ -115,7 +115,7 @@ class StopRunAPIView(APIView):
         run.distance = distance
         run.status = 'finished'
         run.save()
-
+        qs = Position.objects.filter(run_id=run_id)
         result = qs.filter(run=run_id).aggregate(max_value=Max('date_time'), min_value=Min('date_time'))
         try:
             time_difference = (result['max_value'] - result['min_value']).total_seconds()
